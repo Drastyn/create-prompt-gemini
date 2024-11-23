3853ffbf78ef703f3daaf8e553160751b2e7ba21
Drastyn <44484749+Drastyn@users.noreply.github.com>
2024-11-23 11:59:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ from os import getenv, path, popen
 import sys
 import google.generativeai as genai
 
+
 google_api_key = getenv('GOOGLE_API_KEY')
-style = getenv('STYLE')
 output_file_name = getenv('OUTPUT_FILE_NAME')
 
+
 genai.configure(api_key=google_api_key)
-context = "Tu objetivo principal es dado un concepto y un estilo, generes una descripcion de un tatuaje que pueda ser introducida como prompt en un modelo texto a imagen. La desripcion debe ser concreta en no mas de 100 palabras y que pueda ser expresada mediante una imagen."
+context = "Tu objetivo principal es dado un concepto y un estilo, generes una descripcion de un tatuaje que pueda ser introducida como prompt en un modelo texto a imagen. La desripcion debe ser concreta en no mas de 100 palabras y que pueda ser expresada mediante una imagen. "
 model = genai.GenerativeModel('gemini-pro')
 
+
 def main():
-    prompt = checkInput(sys.argv)
+    prompt = prepare_prompt(sys.argv)
     prompt = context + prompt
     response = model.generate_content(prompt).__getattribute__("text")
     popen(f"echo \"{response}\" >> {output_file_name}")
 
-def checkInput(arguments):
-    prompt = ""
-    if(path.isfile(arguments[1])):
-        prompt = popen(f"cat {arguments[1]}").read().replace("\n", "\\n")
-    else:
-        prompt = arguments[1] + arguments[2]
+
+def prepare_prompt(arguments):
+    concept = arguments[1]
+    style = arguments[2]
+    prompt = f"Concepto: {concept}, Estilo: {style}"
     return prompt
 
 main()
