c65bfe4f5c05234a6ec212e24edf8d3d3125ee58
Drastyn <44484749+Drastyn@users.noreply.github.com>
2024-11-27 12:57:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import sys
 import google.generativeai as genai
 
 
-google_api_key = getenv('GOOGLE_API_KEY')
-output_file_name = getenv('OUTPUT_FILE_NAME')
+GOOGLE_API_KEY = getenv('GOOGLE_API_KEY')
+OUTPUT_FILE_NAME = getenv('OUTPUT_FILE_NAME')
 
 
-genai.configure(api_key=google_api_key)
+genai.configure(api_key=GOOGLE_API_KEY)
 context = "Tu objetivo principal es, dado un concepto y un estilo, generar una descripción de un tatuaje que pueda ser introducida como prompt en un modelo texto a imagen. La descripción debe ser concreta en no más de 100 palabras y que pueda ser expresada mediante una imagen. "
 model = genai.GenerativeModel('gemini-pro')
 
@@ -16,7 +16,7 @@ def main():
     prompt = prepare_prompt(sys.argv)
     prompt = context + prompt
     response = model.generate_content(prompt).__getattribute__("text")
-    popen(f"echo \"{response}\" >> {output_file_name}")
+    popen(f"echo \"{response}\" >> {OUTPUT_FILE_NAME}")
 
 
 def prepare_prompt(arguments):
